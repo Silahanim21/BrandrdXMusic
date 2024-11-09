e777e8c45dedcfaaa0e7401d8ec45c2461baa917
Silahanim21 <turkcell63632@gmail.com>
2024-11-09 14:53:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Mehmetkurt676767/BrandrdXMusic",
+    "https://github.com/Silahanim21/BrandrdXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
